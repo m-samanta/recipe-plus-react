e2b03b3d76be846679a999f3a2705b27da4d2994
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-02-29 13:22:13 -0500

    Nav + imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
     <>
-
+      <Router>
+        <Nav />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,77 @@
+import { FontAwesomeIcon } from  '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
+import React from 'react';
+import LandingBackground from "../assets/food-header.jpg";
+import PageLogo from "../assets/logo-white.png";
+
+
+
+const Nav = () => {
+    return (
+        <section id="landing">
+        <div className="recipe__background">
+          <figure className="recipe__background__wrapper">
+            <div className="nav__content">
+              <div className="nav__left">
+                <figure className="nav__logo__wrapper">
+                  <img src={PageLogo} alt="" className="nav__logo" />
+                </figure>
+                <a href="" className="nav__link">Home</a>
+                <a href="" className="nav__link">Contact</a>
+              </div>
+              <div className="nav__right">
+                <div className="nav__input__wrapper">
+                  <input
+                    type="text"
+                    className="nav__input"
+                    placeholder="Find a Recipe"
+                  />
+                  <i className="nav__search"> 
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </i>
+                </div>
+                <i
+                    className="nav__user"> <FontAwesomeIcon icon={faCircleUser} />
+                  </i>
+              </div>
+            </div>
+            <img
+              src={LandingBackground}
+              alt=""
+              className="recipe__background__img"
+            />
+            <div className="recipe__background__text">
+              <h1 className="recipe__background__title">
+                Recipe <span className="textcolor">+</span>
+              </h1>
+              <h3 className="recipe__background__para">
+                With over <span className="textcolor">100</span> recipes, the choice
+                for <span className="textcolor">your next meal</span> is endless!
+              </h3>
+              <div className="recipe__background__search">
+                <div className="recipe__input__wrapper form__submit">
+                  <select className="nav__input__filter">
+                    <option value="" className="nav__input__filter__option meal__name">Search meal by name</option>
+                    <option value="" className="nav__input__filter__option meal__letter">List meals by first letter</option>
+                    <option value="" className="nav__input__filter__option meal__area">Filter by Area</option>
+                    <option value="" className="nav__input__filter__option meal__category">Filter by Category</option>
+                  </select>
+                  <input
+                    type="text"
+                    className="recipe__input"
+                    placeholder="Find a Recipe"
+                  />
+                  <i
+                    className="recipe__search"> <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </i>
+                </div>
+              </div>
+            </div>
+          </figure>
+        </div>
+      </section>
+    );
+}
+
+export default Nav;
