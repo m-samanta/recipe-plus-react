905855bcb90ce1b5eb9219812939a844705b1cc8
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-03-01 14:42:33 -0500

    Skeleton Banner edited

--- src/components/ui/RecipeBannerSkeleton.jsx
+++ src/components/ui/RecipeBannerSkeleton.jsx
@@ -11,34 +11,11 @@ import Img1 from "../../assets/postertest1.jpg"
 const RecipeBannerSkeleton = () => {
     return (
         <div>
-            <div className="recipe">
-        <figure className="recipe__img__wrapper">
-          <img src={Img1} alt="" className="recipe__img" />
-          <h3 className="recipe__info__title">Img</h3>
-          <div className="recipe__info__list">
-            <div className="recipe__info recipe__info1">
-              <i className="recipe__info__icon">
-                <FontAwesomeIcon icon={faEarthAmericas} />
-              </i>
-              <p className="recipe__info__text">img</p>
-            </div>
-            <div className="recipe__info recipe__info2">
-              <i className="recipe__info__icon">
-                <FontAwesomeIcon icon={faStar} />
-              </i>
-              <p className="recipe__info__text">img</p>
-            </div>
-            <div className="recipe__info recipe__info3">
-              <i className="recipe__info__icon">
-                <FontAwesomeIcon icon={faList} />
-              </i>
-              <p className="recipe__info__text">
-                img
-              </p>
-            </div>
-          </div>
+            <div className="recipe recipe--skeleton">
+        <figure className="recipe__img__wrapper recipe__img__wrapper--skeleton">
+          <div className="recipe__img recipe__img--skeleton" />
         </figure>
-        <h4 className="recipe__title">img</h4>
+        <h4 className="recipe__title recipe__title--skeleton">.</h4>
       </div>
         </div>
     );
