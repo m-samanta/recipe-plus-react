6f0d510d2262463c612b30e927cb82a6f5f91b42
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-03-07 17:13:15 -0500

    Added loading skeleton timeout

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,10 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import RecipeBanner from "./ui/RecipeBanner";
 import RecipeBannerSkeleton from "./ui/RecipeBannerSkeleton";
 
 const RecipesList = () => {
   const { recipes, searchMade, searchQuery } = useContext(AppContext);
+  const [showBanners, setShowBanners] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowBanners(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
@@ -13,25 +21,27 @@ const RecipesList = () => {
           <div className="row">
             <div className="recipes__content">
               <div className="recipes__top">
-                {}
                 {searchMade && (
                   <>
                     <h2 className="recipes__top__title">Search results for:</h2>
                     <h2 className="recipe__search__result">"{searchQuery}"</h2>
                   </>
                 )}
-                {}
                 {!searchMade && (
                   <h2 className="recipes__top__title_landing">Top searches:</h2>
                 )}
               </div>
               <div className="recipes__list">
-                {recipes?.length > 0
-                  ? recipes
-                      .slice(0, 6)
-                      .map((recipe) => (
-                        <RecipeBanner key={recipe.idMeal} recipe={recipe} />
-                      ))
+                {showBanners
+                  ? recipes?.length > 0
+                    ? recipes
+                        .slice(0, 6)
+                        .map((recipe) => (
+                          <RecipeBanner key={recipe.idMeal} recipe={recipe} />
+                        ))
+                    : new Array(6)
+                        .fill(0)
+                        .map((_, index) => <RecipeBannerSkeleton key={index} />)
                   : new Array(6)
                       .fill(0)
                       .map((_, index) => <RecipeBannerSkeleton key={index} />)}
