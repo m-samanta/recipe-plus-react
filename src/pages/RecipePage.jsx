36604437bf0a3fdec70ecc685982bc5fbe263665
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-03-12 18:50:28 -0400

    Recipepage Skeleton Functionality

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,12 +1,22 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { useParams } from "react-router-dom";
 import RecipesList from "../components/RecipesList";
+import RecipePageSkeleton from "../components/RecipePageSkeleton";
 
 const RecipePage = () => {
   const { recipes } = useContext(AppContext);
   const { idMeal } = useParams();
   const selectedRecipe = recipes.find((recipe) => recipe.idMeal === idMeal);
+  const [showSkeleton, setShowSkeleton] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSkeleton(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -14,98 +24,107 @@ const RecipePage = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="recipe__page__wrapper">
-          {selectedRecipe && (
-            <>
-              <figure className="recipe__page__img__wrapper">
-                <img
-                  className="recipe__page__img"
-                  src={selectedRecipe.strMealThumb}
-                  alt=""
-                />
-              </figure>
-              <div className="recipe__page__info__row__1">
-                <h2 className="recipe__page__title">
-                  {selectedRecipe.strMeal}
-                </h2>
-                <div className="recipe__page__info">
-                  <div className="recipe__page__text__container__1">
-                  <h2 className="recipe__page__base__text">Location: </h2>
-                  <h2 className="recipe__page__text"> {selectedRecipe.strArea}
+      {showSkeleton? (
+        <RecipePageSkeleton />
+        ) : (
+        <div className="container">
+          <div className="recipe__page__wrapper">
+            {selectedRecipe && (
+              <>
+                <figure className="recipe__page__img__wrapper">
+                  <img
+                    className="recipe__page__img"
+                    src={selectedRecipe.strMealThumb}
+                    alt=""
+                  />
+                </figure>
+                <div className="recipe__page__info__row__1">
+                  <h2 className="recipe__page__title">
+                    {selectedRecipe.strMeal}
                   </h2>
-                  </div>
-                  <div className="recipe__page__text__container__1">
-                  <h2 className="recipe__page__base__text">Category: </h2>
-                  <h2 className="recipe__page__text"> {selectedRecipe.strCategory}
-                  </h2>
-                  </div>
-                  <div className="recipe__page__text__container__2">
-              <h2 className="recipe__page__base__text">Ingredients:</h2>
-              <ul>
-                {Object.keys(selectedRecipe)
-                  .filter(
-                    (key) =>
-                      key.startsWith("strIngredient") && selectedRecipe[key]
-                  )
-                  .map((key) => (
-                    <li key={key}>
-                      {selectedRecipe[key]} - {""}
-                      {selectedRecipe[`strMeasure${key.slice(13)}`]}
-                    </li>
-                  ))}
-              </ul>
+                  <div className="recipe__page__info">
+                    <div className="recipe__page__text__container__1">
+                      <h2 className="recipe__page__base__text">Location: </h2>
+                      <h2 className="recipe__page__text">
+                        {" "}
+                        {selectedRecipe.strArea}
+                      </h2>
+                    </div>
+                    <div className="recipe__page__text__container__1">
+                      <h2 className="recipe__page__base__text">Category: </h2>
+                      <h2 className="recipe__page__text">
+                        {" "}
+                        {selectedRecipe.strCategory}
+                      </h2>
+                    </div>
+                    <div className="recipe__page__text__container__2">
+                      <h2 className="recipe__page__base__text">Ingredients:</h2>
+                      <ul>
+                        {Object.keys(selectedRecipe)
+                          .filter(
+                            (key) =>
+                              key.startsWith("strIngredient") &&
+                              selectedRecipe[key]
+                          )
+                          .map((key) => (
+                            <li key={key}>
+                              {selectedRecipe[key]} - {""}
+                              {selectedRecipe[`strMeasure${key.slice(13)}`]}
+                            </li>
+                          ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="recipe__page__info__row__2">
-              <h2 className="recipe__page__base__text">
-              Instructions:
-              </h2>
-              <div className="recipe__page__para__wrapper">
-  {selectedRecipe.strInstructions.split('.').map((sentence, index) => (
-    <p key={index} className="recipe__page__para">
-      {sentence.trim()}
-    </p>
-  ))}
-</div>
+                <div className="recipe__page__info__row__2">
+                  <h2 className="recipe__page__base__text">Instructions:</h2>
+                  <div className="recipe__page__para__wrapper">
+                    {selectedRecipe.strInstructions
+                      .split(".")
+                      .map((sentence, index) => (
+                        <p key={index} className="recipe__page__para">
+                          {sentence.trim()}
+                        </p>
+                      ))}
+                  </div>
 
-              {selectedRecipe.strYoutube && (
-                <div className="recipe__page__video">
-                  <h2 className="recipe__page__base__text">
-                  Youtube Video:
-                  </h2>
-                  <br />
-                  <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${
-                      selectedRecipe.strYoutube.split("v=")[1]
-                    }`}
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
-                  ></iframe>
+                  {selectedRecipe.strYoutube && (
+                    <div className="recipe__page__video">
+                      <h2 className="recipe__page__base__text">
+                        Youtube Video:
+                      </h2>
+                      <br />
+                      <iframe
+                        width="560"
+                        height="315"
+                        src={`https://www.youtube.com/embed/${
+                          selectedRecipe.strYoutube.split("v=")[1]
+                        }`}
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  )}
+                  {selectedRecipe.strSource && (
+                    <h2 className="recipe__page__base__text">
+                      Source:{" "}
+                      <a
+                        href={selectedRecipe.strSource}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {selectedRecipe.strSource}
+                      </a>
+                    </h2>
+                  )}
                 </div>
-              )}
-              {selectedRecipe.strSource && (
-                <h2 className="recipe__page__base__text">
-                  Source:{" "}
-                  <a
-                    href={selectedRecipe.strSource}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {selectedRecipe.strSource}
-                  </a>
-                </h2>
-              )}
-              </div>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      )}
       <RecipesList />
     </>
   );
